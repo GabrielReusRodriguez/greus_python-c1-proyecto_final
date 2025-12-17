1431dc6629abbcf9b4a8d4e985965eb4d544cbe6
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2025-12-17 22:50:31 +0100

    Se concatenan los comandos de instalacion venv y requisitos con && para que al fallar uno, ya no ejecute el siguiente

--- bin/deploy_env.sh
+++ bin/deploy_env.sh
@@ -2,7 +2,6 @@
 
 echo "Deploy del entorno Proyecto Final de la microcredencial C1 de Pyhton UOC de Greusr"
 
-VENV_PATH ='./.venv'
+VENV_PATH='./.venv'
 
-python3 -m venv ${VENV_PATH}
-${VENV_PATH}/bin/pip3 install -r ./requirements.txt
+python3 -m venv ${VENV_PATH} && bash -c "${VENV_PATH}/bin/pip3 install -r ./requirements.txt"
